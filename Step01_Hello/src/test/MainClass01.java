f91c03a3672cc85095a18c1c00774b83de646e5d
홍주 <@DESKTOP-9F9NLNN>
2020-12-15 19:50:32 +0900

    Step01_Hello 프로젝트 추가함

---
+++ Step01_Hello/src/test/MainClass01.java
@@ -0,0 +1,16 @@
+package test;
+
+//MainClass 라는 이름의 클래스(Class)
+public class MainClass01 {
+	
+	// main 이라는 이름의 메소드(Method)
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+		
+	}
+}
+/*
+ * Class 란 무엇인가?
+ * Method 란 무엇인가?
+ */
---
+++ Step01_Hello/src/test/MainClass02.java
@@ -0,0 +1,19 @@
+package test;
+
+public class MainClass02 {
+	
+	//main 메소드 - run 했을 때 실행의 흐름이 시작되는 특별한 Method
+	public static void main(String[] args) {
+		
+		System.out.println("하나");
+		System.out.println("둘");
+		System.out.println("셋");
+		
+	}
+}
+/* 
+ * 알아가기
+ * 1. public 이란 무엇인가?
+ * 2. static 이란 무엇인가?
+ * 3. void 란 무엇인가?
+ */
