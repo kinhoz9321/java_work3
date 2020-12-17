558e5cdbeaa511af4bda9770e4a64ecccb6f89a0
kimhongju <kinhoz9321@gmail.com>
2020-12-17 12:51:36 +0900

    Step13_UtilClass Main01 추가함 (테스트)

---
+++ Step13_UtilClass/src/test/main/MainClass01.java
@@ -0,0 +1,15 @@
+package test.main;
+
+import java.util.Scanner;
+
+public class MainClass01 {
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("입력 : ");
+		
+		//nextLine() 메소드는 Enter를 치기 전까지 쓴 문자열을 모두 리턴한다.
+		String line=scan.nextLine();
+		System.out.println("앵무새 : "+line);
+	}
+}
