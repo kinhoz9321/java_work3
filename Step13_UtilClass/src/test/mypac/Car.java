e9e3ceac104480cc04d41de58f9cc68bcdf5043a
kimhongju <kinhoz9321@gmail.com>
2020-12-17 22:43:27 +0900

    Step13_UtilClass 프로젝트 추가함

---
+++ Step13_UtilClass/src/test/mypac/Car.java
@@ -0,0 +1,31 @@
+package test.mypac;
+
+public class Car {
+	
+	//필드
+	private String name;
+	
+	//디폴트 생성자
+	public Car() {
+		
+	}
+	
+	//생성자
+	public Car(String name) {
+		this.name=name;
+	}
+	
+	//setter, getter 
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	//메소드
+	public void drive() {
+		System.out.println(this.name+" 이(가) 달려요!");
+	}
+}
